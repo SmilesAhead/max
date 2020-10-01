612f0824bb2ced80b9f435541cecf84a34111aa0
SmilesAhead <shah.abdali.alig@gmail.com>
2020-10-01 12:04:47 +0530

    time added in UI. Data backup integrated.

--- index.js
+++ index.js
@@ -3,11 +3,21 @@ var client = new ModbusRTU();
 var express = require("express");
 var app = express();
 var path = require("path");
+var db = require('./database');
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 
-client.connectRTUBuffered("COM8", { baudRate: 9600, parity: "none" });
+client.connectRTUBuffered("COM5", { baudRate: 9600, parity: "none" });
+
+db.execute('CREATE TABLE IF NOT EXISTS parameters (_id INT PRIMARY KEY AUTO_INCREMENT, temp VARCHAR(5), hum VARCHAR(5), pm2_5 VARCHAR(5), pm10 VARCHAR(5), co2 VARCHAR(5), voc VARCHAR(5), ts DATETIME DEFAULT CURRENT_TIMESTAMP)')
+  .then(result => {
+    console.log('Database Synced')
+  })
+  .catch(error => {
+    console.log(error)
+  })
 
 function connect() {
-  client.connectRTUBuffered("COM8", { baudRate: 9600, parity: "none" });
+  client.connectRTUBuffered("COM5", { baudRate: 9600, parity: "none" });
 }
 client.setTimeout(5000);
 
@@ -28,6 +38,49 @@ app.get("/read/:id", function(req, res) {
   });
 });
 
-app.listen(3001, function() {
-  console.log("listening on localhost:3001");
+app.get("/savedata", (req, res) => {
+    console.log("ees:" + req.query)
+    var temp = req.query.temp
+    var hum = req.query.hum
+    var pm2_5 = req.query.pm2_5
+    var pm10 = req.query.pm10
+    var co2 = req.query.co2
+    var voc = req.query.voc
+    db.execute("INSERT INTO parameters VALLUES(NULL, '" + temp+ "', '" + hum + "', '" + pm2_5 + "', '" + pm10 + "', '" + co2 + "', '" + voc + "', NULL)")
+      .then(() => {
+        res.json('added')
+      })
+      .catch(error => {
+        res.json('db error')
+      })
+})
+
+app.get("/getdata", (req, res) => {
+    db.execute('SELECT ts, temp, hum, pm2_5, pm10, co2, voc FROM parameters')
+      .then(result => {
+        const csvWriter = createCsvWriter({
+          path: "aqm_data.csv",
+          header: [
+            { id: "ts", title: "Time Stamp" },
+            { id: "temp", title: "Temperature(*C)" },
+            { id: "hum", title: "Humidity(%)" },
+            { id: "pm2_5", title: "PM2.5(PPM)" },
+            { id: "pm10", title: "PM10(PPM)" },
+            { id: "co2", title: "CO2(PPM)" },
+            { id: "voc", title: "VOC(PPB)" }
+          ]
+        });
+        csvWriter
+          .writeRecords(result[0])
+          .then(() =>
+            res.json('success')
+          );
+      })
+      .catch(error => {
+        console.log(error)
+      })
+})
+
+app.listen(3000, function() {
+  console.log("listening on localhost:3000");
 });
